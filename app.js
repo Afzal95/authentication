d9a1c21da8f754a3b94da51aa429bcf1c4687f7f
afzal95 <afzalpolite@gmail.com>
2020-07-08 08:49:18 +0530

    Removes mongoose encrytion instead add hashing to password

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
-console.log(process.env.API_KEY);
 
 mongoose.connect("mongodb://localhost:27017/usersDB",{useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -21,13 +21,7 @@ const userSchema = new mongoose.Schema({
   password:String
 });
 
-const secret = process.env.SECRET;
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
-
 const User = mongoose.model("User", userSchema);
-
-
 
 
 app.get("/", function(req, res){
@@ -45,7 +39,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -60,7 +54,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email:username}, function(err, foundUser){
     if(err){
